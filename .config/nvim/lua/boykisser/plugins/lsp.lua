1337ca9eb132822e7a3c4b9d689328c17399b58e
dm <>
2025-11-25 19:28:20 +0300

    lsp fix

--- .config/nvim/lua/boykisser/plugins/lsp.lua
+++ .config/nvim/lua/boykisser/plugins/lsp.lua
@@ -1,63 +1,68 @@
 return {
+  -- mason
   {
     "williamboman/mason.nvim",
     opts = {
-        ui = {
-            icons = {
-                package_installed = "✓",
-                package_pending = "➜",
-                package_uninstalled = "✗"
-            }
-        }
+      ui = {
+	icons = {
+	  package_installed = "✓",
+	  package_pending = "➜",
+	  package_uninstalled = "✗"
+	}
+      }
     },
     build = ":MasonUpdate",
     config = function()
       require("mason").setup()
     end,
   },
+
+  -- mason-lspconfig
   {
     "williamboman/mason-lspconfig.nvim",
     dependencies = { "williamboman/mason.nvim" },
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "pyright" }, -- ставим Pyright
-        automatic_installation = true,
+	ensure_installed = { "pyright" },
+	automatic_installation = true,
       })
     end,
   },
+
+  -- новый LSP API
   {
     "neovim/nvim-lspconfig",
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-    },
     config = function()
-      local lspconfig = require("lspconfig")
+      -- Глобальные настройки для всех серверов
+      vim.lsp.config("*", {
+	capabilities = require("cmp_nvim_lsp").default_capabilities(),
+	on_attach = function(client, bufnr)
+	  local keymap = vim.keymap.set
+	  local opts = { buffer = bufnr, silent = true }
 
-      -- Настройки для LSP (например, отключение подсветки при выделении)
-      local on_attach = function(client, bufnr)
-        local opts = { noremap = true, silent = true, buffer = bufnr }
-        local keymap = vim.keymap.set
-        keymap("n", "gd", vim.lsp.buf.definition, opts)
-        keymap("n", "K", vim.lsp.buf.hover, opts)
-        keymap("n", "gi", vim.lsp.buf.implementation, opts)
-        keymap("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        keymap("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-        keymap("n", "gr", vim.lsp.buf.references, opts)
-	keymap("n", "<leader>e", vim.diagnostic.open_float, { noremap = true, silent = true })
-	keymap("n", "[d", vim.diagnostic.goto_prev, { noremap = true, silent = true })
-	keymap("n", "]d", vim.diagnostic.goto_next, { noremap = true, silent = true })
-	keymap("n", "<leader>q", vim.diagnostic.setloclist, { noremap = true, silent = true })
+	  keymap("n", "gd", vim.lsp.buf.definition, opts)
+	  keymap("n", "K", vim.lsp.buf.hover, opts)
+	  keymap("n", "gi", vim.lsp.buf.implementation, opts)
+	  keymap("n", "<leader>rn", vim.lsp.buf.rename, opts)
+	  keymap("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+	  keymap("n", "gr", vim.lsp.buf.references, opts)
 
-
-      end
-
-      -- Настройка Pyright
-      lspconfig.pyright.setup({
-        on_attach = on_attach,
-        capabilities = require("cmp_nvim_lsp").default_capabilities(),
+	  keymap("n", "<leader>e", vim.diagnostic.open_float, opts)
+	  keymap("n", "[d", vim.diagnostic.goto_prev, opts)
+	  keymap("n", "]d", vim.diagnostic.goto_next, opts)
+	  keymap("n", "<leader>q", vim.diagnostic.setloclist, opts)
+	end,
       })
+
+      -- Прогружаем lspconfig, чтобы он прописал RTP серверов
+      require("lspconfig")
+
+      -- Запускаем нужные серверы
+      vim.lsp.enable({ "pyright" })
     end,
   },
+
+  -- CMP
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
@@ -70,41 +75,42 @@ return {
     config = function()
       local cmp = require("cmp")
       cmp.setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"] = cmp.mapping.select_next_item(),
-          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-        }, {
-          { name = "buffer" },
-          { name = "path" },
-        }),
+	snippet = {
+	  expand = function(args)
+	    require("luasnip").lsp_expand(args.body)
+	  end,
+	},
+	mapping = cmp.mapping.preset.insert({
+	  ["<C-Space>"] = cmp.mapping.complete(),
+	  ["<CR>"] = cmp.mapping.confirm({ select = true }),
+	  ["<Tab>"] = cmp.mapping.select_next_item(),
+	  ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+	}),
+	sources = cmp.config.sources({
+	  { name = "nvim_lsp" },
+	  { name = "luasnip" },
+	}, {
+	  { name = "buffer" },
+	  { name = "path" },
+	}),
       })
     end,
   },
+
+  -- venv-selector
   {
     "linux-cultist/venv-selector.nvim",
     dependencies = {
       "neovim/nvim-lspconfig",
-      "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
+      "mfussenegger/nvim-dap",
+      "mfussenegger/nvim-dap-python",
       { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
     },
-    ft = "python", -- Load when opening Python files
-    branch = "regexp", -- This is the regexp branch, use this for the new version
+    ft = "python",
+    branch = "regexp",
     keys = {
-      { ",v", "<cmd>VenvSelect<cr>" }, -- Open picker on keymap
+      { ",v", "<cmd>VenvSelect<cr>" },
     },
-    opts = {
-      -- Your settings go here
-    },
+    opts = {},
   }
 }
