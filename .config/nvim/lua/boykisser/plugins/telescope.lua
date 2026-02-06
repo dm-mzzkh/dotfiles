bf5a266011a00c01fd45766e33f110a890988061
dm <>
2026-02-07 00:44:16 +0300

    fix telescope

--- .config/nvim/lua/boykisser/plugins/telescope.lua
+++ .config/nvim/lua/boykisser/plugins/telescope.lua
@@ -1,37 +1,45 @@
+-- telescope.lua
+
+-- compat for nvim 0.10
+if vim.treesitter
+  and vim.treesitter.language
+  and vim.treesitter.language.get_lang
+  and not vim.treesitter.language.ft_to_lang
+then
+  vim.treesitter.language.ft_to_lang =
+    vim.treesitter.language.get_lang
+end
+
 return {
   {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.5',
     dependencies = {
       'nvim-lua/plenary.nvim',
       {
-	'nvim-telescope/telescope-fzf-native.nvim',
-	build = 'make',
-      }
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make',
+      },
     },
     config = function()
       local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-      vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
+
+      vim.keymap.set('n', '<leader>ff', builtin.find_files)
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep)
+      vim.keymap.set('n', '<leader>fb', builtin.buffers)
+      vim.keymap.set('n', '<leader>fs', builtin.grep_string)
 
       require('telescope').setup {
-	extensions = {
-	  fzf = {
-	    fuzzy = true,
-	    override_generic_sorter = true,
-	    override_file_sorter = true,
-	    case_mode = "smart_case",
-	  }
-	}
+        extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+          },
+        },
       }
 
       require('telescope').load_extension('fzf')
     end,
   },
-  {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate'
-  }
 }
