2c4135d45c6f937e8e18f348dcb774f8e66d2f41
dm <>
2026-02-10 18:31:50 +0300

    - sounds nvim

--- .config/nvim/lua/boykisser/plugins/sounds.lua
+++ .config/nvim/lua/boykisser/plugins/sounds.lua
@@ -1,7 +1,8 @@
 return {
   "jackplus-xyz/player-one.nvim",
+  enabled = false,
   opts = {
-    min_interval = 0.1, -- Increase delay between sounds to 100ms
-    theme = "chiptune",    -- Use the synthesizer sound theme
+    min_interval = 0.1, -- increase delay between sounds to 100ms
+    theme = "chiptune",    -- use the synthesizer sound theme
   }
 }
